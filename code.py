1f8e243d8f47a460da8ec551392733ec478b3d65
AlexYov <57146002+AlexYov@users.noreply.github.com>
2020-10-31 21:17:11 +0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,46 @@
+from time import time
+
+start_time = time()
+
+length = 10
+letters = 'RLRLLRLLRR'
+
+'''первый метод решения'''
+
+#if letters.count('R') > letters.count('L'):  
+    #new_letters = letters.replace('L','R')
+    #print(new_letters)
+   
+#elif letters.count('R') < letters.count('L'):
+    #new_letters = letters.replace('R','L')
+    #print(new_letters)  
+       
+#else:
+    #pass
+    
+'''второй метод решения'''
+
+list_letters = [letter for letter in enumerate(letters)]
+count_R = letters.count('R')
+count_L = letters.count('L')
+
+positions_L = [ number[0] for number in list_letters if number[1] == 'L']
+
+if count_R > count_L:
+    if positions_L[-1] == length-1:
+        print(letters[:positions_L[-1]].replace('L','R')+letters[positions_L[-1]:])
+        
+    else:
+        number_R1 = letters[:positions_L[-1]].count('R')
+        number_R2 = letters[positions_L[-1]:].count('R')
+        if number_R1 > number_R2:
+            new_letters = letters[:positions_L[-1]].replace('L','R')+letters[positions_L[-1]:].replace('R','L')
+            count_changes = letters[:positions_L[-1]].count('L')+letters[positions_L[-1]:].count('R')
+            print(f'Количество изменений {count_changes}')
+            print(new_letters)
+            
+elif count_R < count_L:
+    pass
+        
+end_time = time()
+print(f'Затраченное время {end_time-start_time}')
